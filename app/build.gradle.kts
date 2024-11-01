386cb7dc51dc31b97eb066db90c1a3a62953f5e5
tavim26 <tavimirisan26@gmail.com>
2024-11-01 21:57:14 +0200

    yeah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.mobilebenchmarkapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mobilebenchmarkapp"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
